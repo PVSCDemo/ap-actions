a3a5b90c30c0bd7234732806111cf91642d2d756
nikantonelli <>
2022-11-08 16:53:49 +0000

    Debug

--- close-ap-issue/index.js
+++ close-ap-issue/index.js
@@ -2,27 +2,19 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 (async () => {
-	core.startGroup('Logging of Code');
-	
 	const inputs = {
 		token: core.getInput("token")
 	}
 	const repo = github.context.payload.repository;
 	const data = github.context.payload.client_payload;
 	const octo = github.getOctokit(inputs.token);
-	const q =  "is:issue+label:bug";
+	const q =  "is:issue label:bug";
 	core.debug(`Search using ${q}`);
 	const results = octo.rest.search.issuesAndPullRequests({ q, });
-
 	core.debug(`results: ${JSON.stringify(results)}`)
-	
-	core.endGroup();
 	return 0;
 
 })().catch(ex => {
 	core.setFailed(error.message);
 	core.debug(`Failure: close-ap-issue: ${ex}`);
-	core.endGroup();
 });
-
-
