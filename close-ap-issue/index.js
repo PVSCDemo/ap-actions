d073537f409f34446f21429e53b7c2543b2c90a5
nikantonelli <>
2022-11-08 17:47:40 +0000

    debug

--- close-ap-issue/index.js
+++ close-ap-issue/index.js
@@ -6,7 +6,7 @@ const github = require('@actions/github');
 	core.debug(`Token: ${token}`);
 	const repo = github.context.payload.repository;
 	const data = github.context.payload.client_payload;
-	const octo = github.getOctokit({ token });
+	const octo = github.getOctokit( token );
 	const q =  "is:issue label:bug";
 	core.debug(`Search using ${token} and ${q}`);
 	const results = octo.rest.search.issuesAndPullRequests({ q, });
