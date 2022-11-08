88c4ebbfb9875b71f3364245bd040946dc380faa
nikantonelli <>
2022-11-08 13:57:49 +0000

    Debug 8

--- close-ap-issue/index.js
+++ close-ap-issue/index.js
@@ -3,17 +3,19 @@ const github = require('@actions/github');
 
 async function run() {
 	const inputs = {
-		token: core.getInput("token"),
-		repository: core.getInput("repository"),
-	  };
+		token: core.getInput("token")
+	}
+
 	try {
+		const repo = github.context.payload.repository;
 		const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-		// const octo = new Octokit({
-		// 	auth: inputs.token,
-		// 	baseUrl: inputs.repository
-		// });
-		// const results = octo.rest.search.issuesAndPullRequests("q=label:Defect "+inputs.data.card.id)
+  		console.log(`The event payload: ${payload}`);
+		const octo = new Octokit({
+			auth: inputs.token,
+			baseUrl: repo
+		});
+		const results = octo.rest.search.issuesAndPullRequests("q=label:Defect "+inputs.data.card.id)
+		core.debug(`results: ${JSON.stringify(results)}`)
 
 	} catch (error) {
 		core.debug(core.inspect(error));
