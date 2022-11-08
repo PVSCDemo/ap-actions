56db9168f97574e568c1f255636ad4ab7e6ede86
nikantonelli <>
2022-11-08 13:16:47 +0000

    Put code back after debug

--- close-ap-issue/index.js
+++ close-ap-issue/index.js
@@ -2,5 +2,25 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
+	const inputs = {
+		token: core.getInput("token"),
+		repository: core.getInput("repository"),
+		data: core.getInput("data"),
+	  };
+	  core.debug(`Inputs: ${core.inspect(inputs)}`);
+	try {
+		const octo = new Octokit({
+			auth: inputs.token,
+			baseUrl: inputs.repository
+		});
+		const results = octo.rest.search.issuesAndPullRequests("q=label:Defect "+data.card.id)
+
+	} catch (error) {
+		core.debug(core.inspect(error));
+		core.setFailed(error.message);
+		if (error.message == 'Resource not accessible by integration') {
+			core.error(`See this action's readme for details about this error`);
+		}
+	}
 }
 
